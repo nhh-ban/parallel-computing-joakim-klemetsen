1bfe007e8a99d608ee96ea52473bcd637302be59
joakim-klemetsen <joakim.klemetsen@icloud.com>
2023-10-25 14:29:19 +0200

    Outlining the solution script

---
+++ timer.r
@@ -0,0 +1,24 @@
+# Load packages
+library(tictoc)
+
+
+# --- Method 1: Solution as is ---
+
+
+# ---
+
+# --- Method 2: Parallel computing ---
+
+
+# ---
+
+# --- Method 3: Splitting simulations in multiple cores ---
+
+
+# ---
+
+
+# --- Comparing of methods, which is fastest? ---
+
+
+# ---
